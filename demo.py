02da33c053ef385a0f20913504f78e797a48c749
satya9900 <85668836+satya9900@users.noreply.github.com>
2021-10-11 20:49:17 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,11 @@
+#welcome to python
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if(i>1):
+        print(i)
+print("__________________________________")
+list2=[12,14,-95,3]
+for j in list2:
+    if(j>1):
+        print(j)
+
