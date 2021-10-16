f20fa0912967bca2d01690fa2dab27367b50f11c
satya9900 <85668836+satya9900@users.noreply.github.com>
2021-10-16 10:17:28 +0530

    Create set operations.py

---
+++ set operations.py
@@ -0,0 +1,15 @@
+# defining sets
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+ 
+#union E and N
+print("Union of E and N :",E | N)
+ 
+#ntersection E and N
+print("Intersection of E and N :",E & N)
+ 
+#difference E and N
+print("Difference of E and N :",E - N)
+ 
+#symmetric difference E and N
+print("Symmetric difference of E and N :",E ^ N)
